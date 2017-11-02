a465df23c09520b9186c0d9be08ace694061182c
LukasBarry <Lukasbarry@yahoo.com>
2017-11-02 14:49:33 -0400

    adds logo and fixes formatting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 import Survey from './Survey'
 import './App.css'
-import Jumbotron from 'react-bootstrap/lib/Jumbotron';
+import stealz from './images/stealz_logo.svg'
 
 export default class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <Jumbotron className='header'>Stealz Survey API</Jumbotron>
-        <div>
-          <Survey />
-        </div>
-      </div>
-    )
-  }
+    render () {
+        return (
+            <div className='App clearfix'>
+                <div className='header'>
+                    <img src={stealz} alt='' className='stealz' />
+                </div>
+                <div className='survey'>
+                    <Survey />
+                </div>
+            </div>
+        )
+    }
 }
